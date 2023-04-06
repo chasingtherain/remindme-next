1aa88c405872063dba080a1fa6eca71bba5b4700
ChasingRain <junpeng94@gmail.com>
2023-04-06 17:58:30 +0800

    linked prisma and postgres to app

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head';
 import HeroHome from '../components/landing/Hero';
+// import prisma from '../lib/prisma';
 
-export default function Home({subtitle,textAnimation,title}) {
-
+export default function Home({subtitle,textAnimation,title, users}) {
+  console.log(users)
   return (
     <div className="">
       <Head>
@@ -12,18 +13,31 @@ export default function Home({subtitle,textAnimation,title}) {
 
       <main className='mt-48'>
         <HeroHome subtitle={subtitle} textAnimation={textAnimation} title={title}/>
+        {/* {users.map(user => <p className='text-center'>{user.email}</p>)} */}
       </main>
     </div>
   )
 }
 
 export async function getStaticProps(){
+  // const currentUser = await prisma.user.findMany({
+  //   where: { email: "baba@mail.com" }
+  // });
+
+  // const serializedUserData = currentUser.map(item => {
+  //   return {
+  //     ...item,
+  //     createdAt: item.createdAt.toISOString(),
+  //     updatedAt: item.updatedAt.toISOString()
+  //   };
+  // });
   return {
     props: {
       textAnimation: ['Buy Eggs',2500,'Pick The Kids',2500,'Stretch My Legs',2500],             
       subtitle: `Never forget things that matter to you again.`,
-      title: `Remind me to`
-
+      title: `Remind me to`,
+      // users: serializedUserData,
     }
   }
 }
+
